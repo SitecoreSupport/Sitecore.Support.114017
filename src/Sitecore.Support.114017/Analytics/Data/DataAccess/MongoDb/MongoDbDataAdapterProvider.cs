f0764e50f7148af5dcd907b21fc4011554a2b88b
AlenPelin <AlenPelin@gmail.com>
2016-12-21 14:14:21 +1000

    Implement retryer

---
+++ src/Sitecore.Support.114017/Analytics/Data/DataAccess/MongoDb/MongoDbDataAdapterProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.Support.Analytics.Data.DataAccess.MongoDb
+{
+  using MongoDB.Driver;
+  using Sitecore.Analytics.DataAccess;
+  using Sitecore.Analytics.Model.Entities;
+  using Sitecore.Diagnostics;
+
+  [UsedImplicitly]
+  public class MongoDbDataAdapterProvider : Sitecore.Analytics.Data.DataAccess.MongoDb.MongoDbDataAdapterProvider
+  {
+    public override bool SaveContact(IContact contact, ContactSaveOptions saveOptions)
+    {
+      try
+      {
+        return base.SaveContact(contact, saveOptions);
+      }
+      catch (MongoDuplicateKeyException ex)
+      {
+        Log.Warn($"{typeof(MongoDuplicateKeyException).Name} ocurred in {typeof(MongoDbDataAdapterProvider).Name}.{nameof(SaveContact)}. Message: {ex.Message}", this);
+        Log.Info("Retrying {typeof(MongoDbDataAdapterProvider).Name}.{nameof(SaveContact)}...", this);
+
+        // try to retry                               
+        return base.SaveContact(contact, saveOptions);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.114017/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.114017")]
+[assembly: AssemblyProduct("Sitecore.Support.114017")]
+[assembly: ComVisible(false)]
